648c9f45525d2f5d6c9c0e83421e9ea9637fdcd2
Oksana Starazhok <Oksana_Starazhok@epam.com>
2018-02-16 17:48:45 +0300

    Master:added two fields into Award Class

--- src/award/Award.java
+++ src/award/Award.java
@@ -1,5 +1,7 @@
 package award;
 
+import java.util.List;
+
 /**
  * Award object for a nomination
  */
@@ -16,9 +18,13 @@ public class Award {
      * socioeconomic class in a certain geographic area, usually a country.
      */
     private double soli;
+    private String type;
+    private int id;
+
 
     /**
      * Constructor for award object WITHOUT soli
+     *
      * @param value award value
      */
     public Award(int value) {
@@ -36,12 +42,53 @@ public class Award {
         this.soli = soli;
     }
 
+    /**
+     * Constructor for awards that have id and type
+     * @param value award value
+     * @param type award type
+     * @param id award id
+     */
+    public Award(int value, String type, int id) {
+        this(value);
+        this.type = type;
+        this.id = id;
+    }
+
+
+    public void pintAwards(List<Award> myAwards) {
+        for (Award award:myAwards) {
+            System.out.println(award);
+        }
+            
+        }
+
+
+
+
+
     public int getValue() {
         return value;
     }
 
     public double getSoli() {
         return soli;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
 
